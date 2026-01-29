e295bb20e755810e190b18e946f4c906a944dd14
miruky <h.miyoki@gmail.com>
2026-01-29 09:47:43 +0900

    lib公開APIのまとめを追加

---
+++ src/lib/index.ts
@@ -0,0 +1,20 @@
+export type { Side, Base, Piece, Sq, Hand, Move, Position, Problem } from './types';
+export {
+  N,
+  idx,
+  onBoard,
+  opponent,
+  buildPosition,
+  destinationsFrom,
+  isAttacked,
+  kingSquare,
+  inCheck,
+  makeMove,
+  legalMoves,
+  isCheckmate,
+  givesCheck,
+  emptyHand,
+  clonePosition,
+} from './shogi';
+export { attackerCanMate, findMatingMove, mateDistance, isSolvingMove, bestDefense } from './solve';
+export { PROBLEMS } from './problems';
